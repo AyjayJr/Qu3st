9029e89fb80bfe08e6b6b4395d16abb11f240bd6
chibitalex <54291689+chibitalex@users.noreply.github.com>
2021-11-22 18:55:27 -0500

    beta character creation

---
+++ CharCreate.js
@@ -0,0 +1,34 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+
+function CharCreate() {
+
+  return (
+    <View style={styles.wrap}>
+
+      <Image source={logo} style={{ width: 750, height: 300 }} /> 
+
+    </View>
+  );
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+});
+
+export default CharCreate;
